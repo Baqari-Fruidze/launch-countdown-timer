3fbc8c50871c78a71a25d7da31641ba1e0fde159
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-03-15 22:59:13 +0400

    finished

---
+++ sctipt.js
@@ -0,0 +1,22 @@
+const daysspan = document.querySelector(".days-span");
+const hoursspan = document.querySelector(".hours-span");
+const minutesspan = document.querySelector(".minutes-span");
+const secondsspan = document.querySelector(".seconds-span");
+
+function change() {
+  const targetDate = new Date("september 1,24 22:58:00");
+  const currentDate = new Date();
+  let difference = targetDate - currentDate;
+  let day = Math.floor(difference / 1000 / 60 / 60 / 24);
+  let hour = Math.floor((difference / 1000 / 60 / 60) % 24);
+  let minutes = Math.floor((difference / 1000 / 60) % 60);
+  let seconds = Math.floor((difference / 1000) % 60);
+  daysspan.textContent = String(day).padStart(2, "0");
+  hoursspan.textContent = String(hour).padStart(2, "0");
+  minutesspan.textContent = String(minutes).padStart(2, "0");
+  secondsspan.textContent = String(seconds).padStart(2, "0");
+  if (day === 0 && hour === 0 && minutes === 0 && seconds === 0) {
+    clearInterval(foo);
+  }
+}
+let foo = setInterval(change, 1000);
