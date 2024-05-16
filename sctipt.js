9cbdf17b477407d0c55ac5dfcaa003b4fc5ac93b
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-05-16 23:25:07 +0400

    data fixed

--- sctipt.js
+++ sctipt.js
@@ -4,7 +4,7 @@ const minutesspan = document.querySelector(".minutes-span");
 const secondsspan = document.querySelector(".seconds-span");
 
 function change() {
-  const targetDate = new Date("september 1,24 22:58:00");
+  const targetDate = new Date("september 20,24 00:20:00");
   const currentDate = new Date();
   let difference = targetDate - currentDate;
   let day = Math.floor(difference / 1000 / 60 / 60 / 24);
@@ -15,7 +15,7 @@ function change() {
   hoursspan.textContent = String(hour).padStart(2, "0");
   minutesspan.textContent = String(minutes).padStart(2, "0");
   secondsspan.textContent = String(seconds).padStart(2, "0");
-  if (day === 0 && hour === 0 && minutes === 0 && seconds === 0) {
+  if (day <= 0 && hour <= 0 && minutes <= 0 && seconds <= 0) {
     clearInterval(foo);
   }
 }
